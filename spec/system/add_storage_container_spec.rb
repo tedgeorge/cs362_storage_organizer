d481ae683e5cc3deaac15b1ecd6ba150c2bc2781
Kenney <programmingsammich@gmail.com>
2019-04-24 11:43:23 -0700

    test: add unhappy path

--- spec/system/add_storage_container_spec.rb
+++ spec/system/add_storage_container_spec.rb
@@ -1,5 +1,6 @@
 require "rails_helper"
 
+
 RSpec.describe "adding a storage container", type: :system do 
 	it "allows a user to create a storage container with items" do
 		visit new_storage_container_path
@@ -11,4 +12,10 @@ RSpec.describe "adding a storage container", type: :system do
 		expect(page).to have_content("Red Dress")
 	end
 	
+	it "does not allow a user to create a storage container without a name" do
+		visit new_storage_container_path
+		fill_in "Name", with: ""
+		click_on("Create Storage Container")
+		expect(page).to have_selector(".new_storage_container")
+	end
 end
