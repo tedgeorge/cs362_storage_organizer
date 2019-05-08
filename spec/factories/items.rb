18623cd4909b1d7932ee0b2f60567666ef16c616
Kenney <programmingsammich@gmail.com>
2019-05-08 11:40:58 -0700

    change factory dir name, add item factory

---
+++ spec/factories/items.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  factory :item do
+    sequence(:name) {|n| "Item #{n}"}
+    description { "Lorem Ipsum for #{:name}"}
+
+    factory :packed do
+      packed true
+    end
+
+    factory :not_packed do
+      packed false
+    end
+  end
+end
