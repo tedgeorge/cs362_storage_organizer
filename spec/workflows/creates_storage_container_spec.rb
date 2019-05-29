11fc4f6a51d1b2d605d633fdf1bc50047286ee97
Ted George <tdzilla57@gmail.com>
2019-05-29 11:06:35 -0700

    Add initial implementation to show size on list of storage containers.

--- spec/workflows/creates_storage_container_spec.rb
+++ spec/workflows/creates_storage_container_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
-RSpec.describe CreatesStorageContainer do 
-	it "creates a project given a name" do
+RSpec.describe CreatesStorageContainer do
+	it "creates a storage container given a name" do
 		creator = CreatesStorageContainer.new(name: "Summer Clothes")
 		creator.build
 		expect(creator.storage_container.name). to eq("Summer Clothes")
