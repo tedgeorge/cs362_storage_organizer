ae2a90709c8f67b880d1b2a1e4d98a7334db9d43
Ted George <tdzilla57@gmail.com>
2019-06-03 11:47:30 -0700

    change item_path to items_path

--- spec/system/item_index_spec.rb
+++ spec/system/item_index_spec.rb
@@ -2,6 +2,6 @@ require "rails_helper"
 
 RSpec.describe "Verifies that the item index works", type: :system do
   let(:test_item) { Item.new }
-  visit item_path
+  visit items_path
   expect(page).to have_content("settlers of catan")
 end
