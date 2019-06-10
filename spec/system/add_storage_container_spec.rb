86d6b75c0d6fc44c95ebcd20587eb1eab93bcb90
Ted George <tdzilla57@gmail.com>
2019-06-09 21:32:54 -0700

    fix small error in test suite

--- spec/system/add_storage_container_spec.rb
+++ spec/system/add_storage_container_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe "adding a storage container and item, and performing functions wi
 		expect(page).to have_content("Summer Clothes")
 		visit items_path
 		expect(page).to have_content("Red Dress")
-		pending "Search", with: "Red Dress"
+		pending fill_in "Search", with: "Red Dress"
 		pending expect(page).to have_content("Red Dress")
 	end
 
