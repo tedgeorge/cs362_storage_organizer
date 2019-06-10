457d04d60be4cf11bb8aa77e43cdb8c13be87105
Rasheed <relkassed@gmail.com>
2019-06-10 03:33:14 -0700

    Add basic method for adding multiple items on creation

--- spec/system/add_storage_container_spec.rb
+++ spec/system/add_storage_container_spec.rb
@@ -1,8 +1,8 @@
 require "rails_helper"
 
 
-RSpec.describe "adding a storage container and item, and performing functions with them", type: :system do
-	it "allows a user to create a storage container with items" do
+RSpec.describe "adding a storage container and items", type: :system do
+	it "allows a user to create a storage container with one item" do
 		visit new_storage_container_path
 		fill_in "Name", with: "Summer Clothes"
 		fill_in "Items", with: "Red Dress"
@@ -17,6 +17,23 @@ RSpec.describe "adding a storage container and item, and performing functions wi
 		expect(page).to have_content("Red Dress")
 	end
 
+  it "allows a user to create a storage container with multiple items" do
+		visit new_storage_container_path
+		fill_in "Name", with: "Summer Clothes"
+		fill_in "Items", with: "Red Dress"
+    click_on("+")
+    fill_in "Item 2", with: "White Dress"
+		click_on("Create Storage Container")
+		visit storage_containers_path
+		expect(page).to have_content("Summer Clothes")
+		visit items_path
+		expect(page).to have_content("Red Dress")
+    
+    pending("Search which should be in its own test")
+		fill_in "Search", with: "Red Dress"
+		expect(page).to have_content("Red Dress")
+	end
+  
 	it "does not allow a user to create a storage container without a name" do
 		visit new_storage_container_path
 		fill_in "Name", with: ""
