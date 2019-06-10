c7f16fc1f74cc9b1d348aa3a7e1441824ee803e4
Rasheed <relkassed@gmail.com>
2019-06-09 18:37:24 -0700

    Remove commented code

--- app/controllers/storage_containers_controller.rb
+++ app/controllers/storage_containers_controller.rb
@@ -7,17 +7,6 @@ class StorageContainersController < ApplicationController
 		@storage_containers = StorageContainer.all
 	end
 
-#	def create
-#		@workflow = CreatesStorageContainer.new(name: params[:storage_container] [:name])
-#		@workflow.create
-#		if @workflow.success?
-#			redirect_to storage_containers_path
-#		else
-#			@storage_container = @workflow.storage_container
-#			render :new
-#		end
-#	end
-
 	def create
 		@storage_container = StorageContainer.new(storage_container_params)
     if @storage_container.save
