5db4505ced1981d4ecd2ea27e9dc7123a77d289f
Ted George <tdzilla57@gmail.com>
2019-06-09 20:12:48 -0700

    add item index test to system test, remove item_index_test.rb file

--- spec/system/item_index_spec.rb
+++
@@ -1,9 +0,0 @@
-require "rails_helper"
-
-RSpec.describe "Listing all the items", type: :system do
-  it "lists all the items" do
-    visit items_path
-    pending("expect settlers of catan")
-    expect(page).to have_content("settlers of catan")
-  end
-end
