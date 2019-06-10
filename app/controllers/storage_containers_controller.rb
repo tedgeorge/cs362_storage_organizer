3427660bb91079835a73a1f97a25b48a2ad3588d
Rasheed <relkassed@gmail.com>
2019-06-09 18:31:45 -0700

    Remove workflows from the code base

--- app/controllers/storage_containers_controller.rb
+++ app/controllers/storage_containers_controller.rb
@@ -1,21 +1,34 @@
 class StorageContainersController < ApplicationController
 	def new
 		@storage_container = StorageContainer.new
-    @storage_container.items << Item.new
 	end
 
 	def index
 		@storage_containers = StorageContainer.all
 	end
 
+#	def create
+#		@workflow = CreatesStorageContainer.new(name: params[:storage_container] [:name])
+#		@workflow.create
+#		if @workflow.success?
+#			redirect_to storage_containers_path
+#		else
+#			@storage_container = @workflow.storage_container
+#			render :new
+#		end
+#	end
+
 	def create
-		@workflow = CreatesStorageContainer.new(name: params[:storage_container] [:name])
-		@workflow.create
-		if @workflow.success?
-			redirect_to storage_containers_path
-		else
-			@storage_container = @workflow.storage_container
-			render :new
-		end
+		@storage_container = StorageContainer.new(storage_container_params)
+    if @storage_container.save
+      redirect_to storage_containers_path
+    else
+      render :new
+    end
 	end
+  
+  def storage_container_params
+    params.require(:storage_container).permit(:name, items: [])
+  end
+  
 end
